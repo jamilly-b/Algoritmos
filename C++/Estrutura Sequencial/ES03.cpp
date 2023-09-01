9c4be833fed04d3a10ec30a71d900704b4684ce2
Jamilly Braga <114257647+jamilly-b@users.noreply.github.com>
2023-09-01 14:50:48 -0300

    Create ES03.cpp

---
+++ C++/Estrutura Sequencial/ES03.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cout << "digite um numero: ";
+    cin >> x;
+    cout << "digite outro numero: ";
+    cin >> y;
+    cout << "a soma dos numeros é igual a " << x + y;
+    return 0;
+}
