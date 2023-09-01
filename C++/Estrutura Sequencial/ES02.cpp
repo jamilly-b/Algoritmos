a36091f806cc4eea856cbae99e45e26658e35d22
Jamilly Braga <114257647+jamilly-b@users.noreply.github.com>
2023-09-01 14:52:30 -0300

    Create ES02.cpp

---
+++ C++/Estrutura Sequencial/ES02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "digite um numero: ";
+    cin >> x;
+    cout << "o numero informado foi " << x << ".";
+    return 0;
+}
