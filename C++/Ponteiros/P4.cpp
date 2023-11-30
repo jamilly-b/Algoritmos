85ee27dc8f47fd6b5231a5a32f03fb87ba2fc29a
Jamilly Braga <114257647+jamilly-b@users.noreply.github.com>
2023-11-30 15:59:20 -0300

    Create P4.cpp

---
+++ C++/Ponteiros/P4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*Crie um programa que contenha um array de floats com 10 elementos. 
+Imprima o endereço de cada posição desse array.*/
+
+
+int main()
+{
+    float array[5] = {1, 2, 3, 4, 5};
+    cout << "Endereço inicial: " << array << endl;
+    for(int i = 0; i <= 5; i++){
+        cout << "Endereço do " << array[i] << ": " << array + i << endl;
+    }
+    
+    return 0;
+}
